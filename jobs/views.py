6a82168fbd859c6bd64e6fe9d289a0c5f0b125a1
jamalihassan0307 <jamalihassan0307@gmail.com>
2025-05-10 11:07:14 +0500

    Implement About and Contact pages with corresponding templates and styles; update base template to include new CSS and JS files; refine job model import in views.py.

--- jobs/views.py
+++ jobs/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
-from .models import Job, ContactDetail, User, Role
+from jobs.models import Job
 from django.db.models import Q
 
 def home(request):
